7a66af4b93ee987a6de555bde6c85d9af1099d34
pierremayo <42851003+pierremayo@users.noreply.github.com>
2018-09-07 11:51:38 -0400

    Updated from Brython Server: 9/7/2018 11:51:38 AM

--- calculatepi.py
+++ calculatepi.py
@@ -1,7 +1,7 @@
 """
 calculatepi.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Pierre Mayo
+Credit: I used the Calculate-Pi ReadMe as a reference.
 Assignment:
 
 Write and submit a Python program that computes an approximate value of π by calculating the following sum:
